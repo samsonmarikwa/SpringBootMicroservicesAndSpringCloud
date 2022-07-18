131bcab70758ade65d4d7d2e058441d834386514
Samson Marikwa <samsonmarikwa@outlook.com>
2022-07-17 23:32:47 -0400

    @RequestParam to read request parameters

--- mobile-app-ws/src/main/java/com/samsonmarikwa/appws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/samsonmarikwa/appws/ui/controller/UserController.java
@@ -6,11 +6,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
+	
+	@GetMapping
+	public String getUsers(@RequestParam(value="page", defaultValue="1") int page,
+			@RequestParam(value="limit", defaultValue="50") int limit) {
+		return "get users was called with page=" + page + " and limit=" + limit;
+	}
 	
 	@GetMapping(path="/{userId}")
 	public String getUser(@PathVariable String userId) {
