0eb248e7021f30713537e99724887ef76aafbb03
Samson Marikwa <samsonmarikwa@outlook.com>
2022-07-26 17:06:30 -0400

    Implement attemptAuthentication

--- photoappusers/src/main/java/com/samsonmarikwa/photoappusers/security/WebSecurity.java
+++ photoappusers/src/main/java/com/samsonmarikwa/photoappusers/security/WebSecurity.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.servlet.Filter;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
@@ -16,7 +18,15 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
    @Override
    protected void configure(HttpSecurity http) throws Exception {
       http.csrf().disable();
-      http.authorizeRequests().antMatchers("/**").hasIpAddress(ipAddress);
+      http.authorizeRequests().antMatchers("/**").hasIpAddress(ipAddress)
+                  .and()
+                        .addFilter(getAuthenticationFilter());
       http.headers().frameOptions().disable(); // required to allow h2-console
+   }
+   
+   private Filter getAuthenticationFilter() throws Exception {
+      AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+      authenticationFilter.setAuthenticationManager(authenticationManager());
+      return authenticationFilter;
    }
 }
