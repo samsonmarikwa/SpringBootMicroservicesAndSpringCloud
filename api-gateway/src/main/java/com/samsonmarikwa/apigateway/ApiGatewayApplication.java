ee6ff3e56617ff5fecc1696871c91f039a964adf
Samson Marikwa <samsonmarikwa@outlook.com>
2022-07-29 13:07:55 -0400

    Spring Cloud Bus - replicate config updates to all microservices - getting status 405

--- api-gateway/src/main/java/com/samsonmarikwa/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/samsonmarikwa/apigateway/ApiGatewayApplication.java
@@ -3,6 +3,7 @@ package com.samsonmarikwa.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
