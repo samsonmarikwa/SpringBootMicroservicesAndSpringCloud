b91a2fc0ba51bc86ca535c34718a1258bf35676e
Samson Marikwa <samsonmarikwa@outlook.com>
2022-08-08 00:38:10 -0400

    Add circuit breaker dependency and configure

--- photoappusers/src/main/java/com/samsonmarikwa/photoappusers/PhotoappUsersApplication.java
+++ photoappusers/src/main/java/com/samsonmarikwa/photoappusers/PhotoappUsersApplication.java
@@ -5,6 +5,7 @@ import feign.Logger;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -16,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class PhotoappUsersApplication {
 
 	public static void main(String[] args) {
--- photoappusers/src/main/java/com/samsonmarikwa/photoappusers/data/AlbumsServiceClient.java
+++ photoappusers/src/main/java/com/samsonmarikwa/photoappusers/data/AlbumsServiceClient.java
@@ -2,14 +2,26 @@ package com.samsonmarikwa.photoappusers.data;
 
 import com.samsonmarikwa.photoappusers.ui.model.AlbumResponseModel;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@FeignClient(name = "albums-ws")
+@FeignClient(name = "albums-ws", fallback = AlbumsFallback.class)
 public interface AlbumsServiceClient {
    
-   @GetMapping("/users/{id}/albumss")
+   @GetMapping("/users/{id}/albums")
    public List<AlbumResponseModel> getAlbums(@PathVariable String id);
+}
+
+@Component
+class AlbumsFallback implements AlbumsServiceClient {
+   
+   @Override
+   public List<AlbumResponseModel> getAlbums(String id) {
+      // The fallback method returns an empty list.
+      return new ArrayList<>();
+   }
 }
