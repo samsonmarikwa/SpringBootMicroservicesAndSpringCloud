37e366b239bf8c1f421ceece473a8679bd84683d
Samson Marikwa <samsonmarikwa@outlook.com>
2022-08-09 00:45:12 -0400

    Configure Access to Actuator endpoints

--- photoappusers/src/main/java/com/samsonmarikwa/photoappusers/PhotoappUsersApplication.java
+++ photoappusers/src/main/java/com/samsonmarikwa/photoappusers/PhotoappUsersApplication.java
@@ -5,12 +5,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.client.RestTemplate;
-
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -32,11 +29,11 @@ public class PhotoappUsersApplication {
 		return new BCryptPasswordEncoder();
 	}
 	
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+//	@Bean
+//	@LoadBalanced
+//	public RestTemplate restTemplate() {
+//		return new RestTemplate();
+//	}
 	
 	@Bean
 	Logger.Level feignLoggerLevel() {
